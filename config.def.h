ed3d826f9d2e2ec90d01a7afcc3c2c77c652d96c
Marco Caimi <caimi.marco@protonmail.ch>
2021-08-31 14:53:24 +0200

    Added center patch

--- config.def.h
+++ config.def.h
@@ -7,6 +7,9 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const unsigned int alpha = 0xf0;
 /* window border thickness */
 static unsigned int border_width = 0;
+/* centered patch options */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                 /* minimum width when centered */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
