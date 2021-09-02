c9bc7548a4c1c1db7b497b847eccce185734edea
Marco Caimi <caimi.marco@protonmail.ch>
2021-09-02 11:29:58 +0200

    Updated README, retabbed all source code files

--- config.def.h
+++ config.def.h
@@ -14,27 +14,27 @@ static int min_width = 500;                 /* minimum width when centered */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+  "monospace:size=10"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *dynamic     = NULL;      /* -dy option; dynamic command to run on input change */
 
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+  /*     fg         bg       */
+  [SchemeNorm] = { "#bbbbbb", "#222222" },
+  [SchemeSel] = { "#eeeeee", "#005577" },
+  [SchemeSelHighlight] = { "#ffc978", "#005577" },
+  [SchemeNormHighlight] = { "#ffc978", "#222222" },
+  [SchemeOut] = { "#000000", "#00ffff" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
-	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeSelHighlight] = { OPAQUE, alpha},
-	[SchemeNormHighlight] = { OPAQUE, alpha},
-	[SchemeOut] = { OPAQUE, alpha },
+  [SchemeNorm] = { OPAQUE, alpha },
+  [SchemeSel] = { OPAQUE, alpha },
+  [SchemeSelHighlight] = { OPAQUE, alpha},
+  [SchemeNormHighlight] = { OPAQUE, alpha},
+  [SchemeOut] = { OPAQUE, alpha },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
