30e67d4d1a0a5d38d8498dd619e3971ea01e2426
Marco Caimi <mcaimi@redhat.com>
2024-02-22 12:00:52 +0100

    Upstream Patch: fix BadMatch error when embedding on some windows
    When embedded into another window, dmenu will fail with the BadMatch
    error if that window have not the same colormap/depth/visual as the
    root window.
    
    That happens because dmenu inherits the colormap/depth/visual from
    its parent, but draws on a pixmap created based on the root window
    using a GC created for the root window (see drw.c).  A BadMatch will
    occur when copying the content of the pixmap into dmenu's window.
    
    A solution is to create dmenu's window inside root and then reparent
    it if embeded.
    
    See this mail[1] on ports@openbsd.org mailing list for context.
    
    [1]: https://marc.info/?l=openbsd-ports&m=168072150814664&w=2k
    
    Author: Lucas de Sena <lucas@seninha.org>

--- dmenu.c
+++ dmenu.c
@@ -959,7 +959,7 @@ setup(void)
   swa.border_pixel = 0;
   swa.colormap = cmap;
   swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
-  win = XCreateWindow(dpy, parentwin, x, y - (topbar ? 0 : border_width * 2), mw - border_width * 2, mh, border_width,
+  win = XCreateWindow(dpy, root, x, y - (topbar ? 0 : border_width * 2), mw - border_width * 2, mh, border_width,
                       depth, CopyFromParent, visual, CWOverrideRedirect | CWBackPixel | CWBorderPixel | CWColormap | CWEventMask, &swa);
 
   if (border_width)
@@ -977,6 +977,7 @@ setup(void)
 
   XMapRaised(dpy, win);
   if (embed) {
+    XReparentWindow(dpy, win, parentwin, x, y);
     XSelectInput(dpy, parentwin, FocusChangeMask | SubstructureNotifyMask);
     if (XQueryTree(dpy, parentwin, &dw, &w, &dws, &du) && dws) {
       for (i = 0; i < du && dws[i] != win; ++i)
